42626c039c375827d0a04a18d447f78fed4aa583
Zaidan <znaufalilmi@gmail.com>
2023-10-27 17:07:42 +0700

    uba models

--- main/forms.py
+++ main/forms.py
@@ -4,4 +4,4 @@ from main.models import Buku
 class BukuForm(ModelForm):
     class Meta:
         model = Buku
-        fields = ["judul", "jumlah_halaman", "bahasa", "penulis", "penerbit", "jumlah_review", "rating", "gambar", "tanggal_terbit", "isi_buku"]
+        fields = ["judul", "jumlah_halaman", "bahasa", "penulis", "penerbit", "jumlah_review", "rating", "gambar", "tanggal_terbit"]
