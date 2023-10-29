02a5acdd52710124f9f1eb9f8a36243aff4df6d2
Jason <118716513+Fec16@users.noreply.github.com>
2023-10-30 00:01:26 +0700

    Update urls.py

--- modul_favorites/urls.py
+++ modul_favorites/urls.py
@@ -1,10 +1,4 @@
 from django.urls import path
-from main.views import show_main, create_buku, show_preview, show_xml, show_json, show_xml_by_id, show_json_by_id 
-from main.views import register #sesuaikan dengan nama fungsi yang dibuat
-from main.views import login_user
-from main.views import logout_user
-from main.views import edit_buku
-from main.views import delete_buku
 from modul_favorites.views import *
 
 app_name = 'modul_favorites'
@@ -13,4 +7,5 @@ urlpatterns = [
     path('show_favorites/', show_favorites, name='show_favorites'),
     path('add_favorites/<int:id>', add_favorites, name='add_favorites'),
     path('remove_favorites/<int:id>', remove_favorites, name='remove_favorites'),
+    path('toggle_favorites/', toggle_favorites, name='toggle_favorites'),
 ]
