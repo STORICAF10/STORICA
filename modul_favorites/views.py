399d3c0d5f0a63fe927b24ba08b565704528a484
Jason <118716513+Fec16@users.noreply.github.com>
2023-10-30 00:00:39 +0700

    Update views.py

--- modul_favorites/views.py
+++ modul_favorites/views.py
@@ -2,7 +2,7 @@ from django.urls import reverse
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
-
+from django.http import JsonResponse
 
 from main.models import Buku
 from main.models import BukuKreasi
@@ -32,3 +32,22 @@ def remove_favorites(request,id):
     buku.buku_favorites.remove(user)
     buku.save()
     return HttpResponseRedirect(reverse('modul_favorites:show_favorites'))
+
+@login_required
+def toggle_favorites(request):
+    if request.method == 'POST':
+        id = request.POST.get('id')
+        action = request.POST.get('action')
+        buku = Buku.objects.get(pk=id)
+        user = request.user
+
+        if action == 'add':
+            buku.buku_favorites.add(user)
+            buku.save()
+            return JsonResponse({'status': 'added'})
+        elif action == 'remove':
+            buku.buku_favorites.remove(user)
+            buku.save()
+            return JsonResponse({'status': 'removed'})
+
+    return JsonResponse({'status': 'error'})
