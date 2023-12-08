01674582c94609047a2f903630184c30c419bb0a
Edward-Salim <edwardsalim29@gmail.com>
2023-12-09 02:00:45 +0700

    Keperluan Modul Baca Flutter

--- modul_baca/urls.py
+++ modul_baca/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-from modul_baca.views import json_komentar, json_komentar_kreasi, show_create_komentar, show_create_komentar_kreasi
-from modul_preview.views import show_bacaan
+from modul_baca.views import create_komentar_flutter, json_komentar, json_komentar_kreasi, show_create_komentar, show_create_komentar_kreasi
 
 app_name = 'modul_baca'
 
@@ -9,4 +8,5 @@ urlpatterns = [
     path('komentar-karya/<int:buku_id>/', show_create_komentar_kreasi, name='show_create_komentar_kreasi'),
     path('json-komentar/<int:buku_id>/', json_komentar, name='json_komentar'),
     path('json-komentar-kreasi/<int:buku_id>/', json_komentar_kreasi, name='json_komentar_kreasi'),
+    path('create-komentar-flutter/', create_komentar_flutter, name='create_komentar_flutter'),
 ]
--- modul_baca/views.py
+++ modul_baca/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
-from main.models import Buku, BukuKreasi
-from django.shortcuts import redirect
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from modul_baca.forms import KomentarForm, KomentarKreasiForm
 from django.http import HttpResponseRedirect
 from django.http import HttpResponse
 from django.core import serializers
+import json
+from django.http import JsonResponse
+from main.models import Buku, BukuKreasi
+from modul_baca.models import Komentar, KomentarKreasi
+from django.views.decorators.csrf import csrf_exempt
 
 
 @login_required(login_url='/login')
@@ -18,7 +21,6 @@ def show_create_komentar(request, buku_id):
         form = KomentarForm(request.POST)
         if form.is_valid():
             komentar = form.save(commit=False)
-            komentar.user = request.user
             komentar.dari_buku = buku
             komentar.save()
             return HttpResponseRedirect(reverse('modul_baca:show_create_komentar', args=[buku_id]))
@@ -37,7 +39,6 @@ def show_create_komentar_kreasi(request, buku_id):
         form = KomentarKreasiForm(request.POST)
         if form.is_valid():
             komentar = form.save(commit=False)
-            komentar.user = request.user
             komentar.dari_buku = buku
             komentar.save()
             return HttpResponseRedirect(reverse('modul_baca:show_create_komentar_kreasi', args=[buku_id]))
@@ -48,10 +49,30 @@ def show_create_komentar_kreasi(request, buku_id):
 
 
 def json_komentar(request, buku_id):
-    komentar_buku = Buku.objects.get(pk=buku_id).komentar.all()
+    komentar_buku = Buku.objects.get(pk=buku_id).komentar.all().order_by('-tgl_komentar')
     return HttpResponse(serializers.serialize("json", komentar_buku))
 
 
 def json_komentar_kreasi(request, buku_id):
-    komentar_buku_kreasi = BukuKreasi.objects.get(pk=buku_id).komentar.all()
+    komentar_buku_kreasi = BukuKreasi.objects.get(pk=buku_id).komentar.all().order_by('-tgl_komentar')
     return HttpResponse(serializers.serialize("json", komentar_buku_kreasi))
+
+
+@csrf_exempt
+def create_komentar_flutter(request):
+    if request.method == 'POST':
+        
+        data = json.loads(request.body)
+        buku = Buku.objects.get(pk=data["dari_buku"])
+
+        new_komentar = Komentar.objects.create(
+            isi_komentar = data["isi_komentar"],
+            rating = int(data["rating"]),
+            dari_buku = buku,
+        )
+
+        new_komentar.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
