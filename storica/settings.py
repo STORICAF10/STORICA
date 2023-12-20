2b88c44831c9cb074be0b873c6882d6ed7be9a31
Zaidan <znaufalilmi@gmail.com>
2023-12-20 17:41:27 +0700

    whitenoise

--- storica/settings.py
+++ storica/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',    
     'main',
     'modul_baca',
     'modul_preview',
@@ -56,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'storica.urls'
